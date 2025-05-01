f7a8aef9b6fcd00dd297d41a18a9053a27a3f2ef
ideariumstudio <ideariums@gmail.com>
2025-04-30 23:05:52 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+// Exemplo de futura animação ou scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
